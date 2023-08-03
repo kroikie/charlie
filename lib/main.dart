3664982cc969901907c99c6574ac87b1564ca0e4
Arthur Thompson <arthur.thompson.jr@gmail.com>
2023-08-03 20:13:32 +0000

    nit: add very to title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'I Am Working Very Well'),
+      home: const MyHomePage(title: 'I Am Working Very Very Well'),
     );
   }
 }
